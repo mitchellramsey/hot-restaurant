16f83e748376fd77d271545033c7a0e41273eb58
Mitchell Ramsey <mitchellramsey1992@gmail.com>
2018-03-14 20:09:13 -0400

    form is now working

--- server.js
+++ server.js
@@ -38,6 +38,24 @@ app.get("/api/reservations", function(req, res) {
     res.json(reservations);
 });
 
+// Create New Characters - takes in JSON input
+app.post("/api/reservations", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body-parser middleware
+    // console.log(req);
+    // console.log(res);
+    var newReservation = req.body;
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    // newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+  
+    // console.log(newcharacter);
+  
+    reservations.push(newReservation);
+  
+    res.json(newReservation);
+  });
+
 
 
 
